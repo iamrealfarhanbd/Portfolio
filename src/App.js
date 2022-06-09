50ba19cc8d218f8a1335c2ea794ea15a6dd3c1ea
iamrealfarhanbd <iamfarhan09@gmail.com>
2022-06-09 20:42:06 +0600

    update

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -38,6 +39,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,88 @@
+import React, { useRef } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import Particle from "../Particle";
+import laptopImg from "../../Assets/about.png";
+import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
+
+function Contact() {
+  const form = useRef();
+
+  const { register, handleSubmit } = useForm();
+
+  const sendEmail = (data) => {
+    emailjs
+      .sendForm(
+        "service_w422wzm",
+        "template_3d288tb",
+        form.current,
+        "WUbPcNX44GE7fiWEZ"
+      )
+      .then(
+        (data) => {
+          console.log(data.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <Container fluid className="about-section">
+      <Particle />
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          >
+            <img src={laptopImg} alt="about" className="img-fluid" />
+          </Col>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <Form ref={form} onSubmit={handleSubmit(sendEmail)} className="text-start">
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Your Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Your Name"
+                  {...register("from_name", { required: true, maxLength: 80 })}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Your Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Email"
+                  {...register("reply_to", {
+                    required: true,
+                    pattern: /^\S+@\S+$/i,
+                  })}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Your Message</Form.Label>
+
+                <Form.Control as="textarea" {...register("message", {})} />
+              </Form.Group>
+              <Form.Control className="my-3" type="submit" />
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Contact;
